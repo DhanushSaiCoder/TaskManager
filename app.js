a1454a172add3a214403f2e514294a5b84d693ec
Dhanush Sai Nayak <dhanushsai1467@gmail.com>
2024-11-21 22:56:30 +0530

    server created-before mongoose

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {console.log('Connected to DB')})
+.catch((err) => {console.error('cannot connect to db',err)});
+
+app.get('/',(req,res) => {
+    res.send('hello Page')
+})
+
+app.listen(PORT, () => { console.log(`Server running on http://localhost:${PORT}`); });
