d3c2f3ca63edd0b6efe3c41c26f72ede05685fea
Dhanush Sai Nayak <dhanushsai1467@gmail.com>
2024-11-22 00:09:07 +0530

    CRUD Tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -27,4 +27,24 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.patch('/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!task) return res.status(404).send('Task not found')
+        res.send(task)
+    } catch (error) {
+        console.error('error while patch', error)
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) return res.status(404).send('Task not found')
+
+        res.status(200).send(task)
+    } catch (err) {
+        console.error('error while deleting task',err)
+    }
+})
 module.exports = router
