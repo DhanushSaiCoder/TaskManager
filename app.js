e6926a3924495001043b199c76f5e9720bd9cbcf
Dhanush Sai Nayak <dhanushsai1467@gmail.com>
2024-11-24 01:08:40 +0530

    app.js

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-const Task = require('./models/Task')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Task = require('./models/Task');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'))
-app.use("/auth", require("./routes/auth"));
+app.use(express.static('public'));
+app.use('/auth', require('./routes/auth'));
 
-//mongodb+srv://<db_username>:<db_password>@taskmanager.8zpj4.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => { console.log('Connected to DB') })
-    .catch((err) => { console.error('cannot connect to db', err) });
+// Ensure correct MONGO_URI
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to DB');
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Cannot connect to db', err);
+    });
 
 app.get('/', (req, res) => {
-    res.send('hello Page')
-})
+    res.send('hello Page');
+});
 
-app.use('/tasks', require('./routes/tasks'))
-
-app.listen(PORT, () => { console.log(`Server running on http://localhost:${PORT}`); });
+app.use('/tasks', require('./routes/tasks'));
